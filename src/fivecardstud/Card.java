5af120fc555c8444318eecef15ad4d23bddd6271
Bradley Moore <BradleyMooreTX@gmail.com>
2013-10-08 15:24:08 -0600

    created card class; looking good so far

---
+++ src/fivecardstud/Card.java
@@ -0,0 +1,29 @@
+package fivecardstud;
+
+public class Card {
+    Card (String number, char suit){
+        String cardValue = number;
+        char cardSuit = suit;
+        int worth;
+        
+        if (Integer.parseInt(number) >= 2 || Integer.parseInt(number) <= 10){
+            worth = Integer.parseInt(number);
+        } else {
+            switch(cardValue){
+                case "A":
+                    worth = 14;
+                    break;
+                case "K":
+                    worth = 13;
+                    break;
+                case "Q":
+                    worth = 12;
+                    break;
+                case "J":
+                    worth = 11;
+                    break;
+            }
+        }
+        
+    }
+}
--- src/fivecardstud/FiveCardStud.java
+++ src/fivecardstud/FiveCardStud.java
@@ -1,19 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package fivecardstud;
 
-/**
- *
- * @author bamoore1
- */
 public class FiveCardStud {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
