1a4822801cd466a6333c837988080ead4f707ea4
Bradley Moore <BradleyMooreTX@gmail.com>
2013-10-10 14:20:19 -0600

    created and started player class

---
+++ src/fivecardstud/Player.java
@@ -0,0 +1,11 @@
+package fivecardstud;
+
+public class Player {
+    String name;
+    Card[] hand;
+    
+    public Player(String name, Card[] hand){
+        this.name = name;
+        this.hand = hand;
+    }
+}
