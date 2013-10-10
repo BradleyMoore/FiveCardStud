386ad23202d392b0a11a10edd5ae52ee379abe0f
Bradley Moore <BradleyMooreTX@gmail.com>
2013-10-10 10:30:39 -0600

    started creating the deck; changed card to hashMap...much simpler

--- src/fivecardstud/Card.java
+++ src/fivecardstud/Card.java
@@ -1,29 +1,17 @@
 package fivecardstud;
-
-public class Card {
-    Card (String number, char suit){
-        String cardValue = number;
-        char cardSuit = suit;
-        int worth;
+import java.util.HashMap;
         
-        if (Integer.parseInt(number) >= 2 && Integer.parseInt(number) <= 10){
-            worth = Integer.parseInt(number);
-        } else {
-            switch(cardValue){
-                case "A":
-                    worth = 14;
-                    break;
-                case "K":
-                    worth = 13;
-                    break;
-                case "Q":
-                    worth = 12;
-                    break;
-                case "J":
-                    worth = 11;
-                    break;
-            }
+public class Card {
+    Card (String value, String suit){
+        
+        HashMap worth = new HashMap<String, Integer>();
+        for (int i=2; i<=10; i++){
+            worth.put(new Integer(i).toString(), i);
         }
+        worth.put("Jack", 10);
+        worth.put("Queen", 11);
+        worth.put("King", 12);
+        worth.put("Ace", 13);
         
     }
 }
