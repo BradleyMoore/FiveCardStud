3d6024cc59b5b896875419f72cc2a0d4f2acd767
Bradley Moore <BradleyMooreTX@gmail.com>
2013-10-10 13:17:24 -0600

    trying to print the shuffled deck. finished deck and card classes

--- src/fivecardstud/FiveCardStud.java
+++ src/fivecardstud/FiveCardStud.java
@@ -3,6 +3,9 @@ package fivecardstud;
 public class FiveCardStud {
 
     public static void main(String[] args) {
-        
+        Deck deck = new Deck();
+        for (int i=0; i<deck.DECK_SIZE; i++){
+            System.out.println((deck.deck[i].rank + " " deck.deck[i].suit));
+        }
     }
 }
