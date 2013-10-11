f5802061b8bb86bdf800fbacfb9f6da29182c141
Bradley Moore <BradleyMooreTX@gmail.com>
2013-10-11 09:21:48 -0600

    added to hand class, it does not extend deck; adding deal method to deck class

--- src/fivecardstud/Deck.java
+++ src/fivecardstud/Deck.java
@@ -1,38 +1,42 @@
 package fivecardstud;
 
 public class Deck {
-    final int DECK_SIZE;
+    int deckSize;
     Card[] deck;
     String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack",
         "Queen", "King", "Ace"};
-    String[] suites = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
     
     public Deck() {
-        this.DECK_SIZE = 52;
-        this.deck = new Card[DECK_SIZE];
+        this.deckSize = 52;
+        this.deck = new Card[deckSize];
         
         // iterate through values and suites to create each card
         int i = 0;
-        for (String suite : suites){
+        for (String suit: suits){
             for (String value : values){
-                deck[i] = new Card(value, suite);
+                deck[i] = new Card(value, suit);
                 i++;
             }
         }
-        shuffleDeck();
     }
     
-    public final Card[] shuffleDeck(){
+    public Card[] shuffle(){
         int rand;
         Card temp;
         
         // swap the card at index i with any later card
-        for (int i=0; i<DECK_SIZE; i++){
-            rand = i + (int)(Math.random() * ((DECK_SIZE-1 - i) + 1));
+        for (int i=0; i<deckSize; i++){
+            rand = i + (int)(Math.random() * ((deckSize-1 - i) + 1));
             temp = deck[i];
             this.deck[i] = deck[rand];
             deck[rand] = temp;
         }
         return deck;
+    }
+    
+    public Card[] deal(int toDeal){
+        int rand;
+        
     }
 }
