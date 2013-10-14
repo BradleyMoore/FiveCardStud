e88e3a219dccdb1e52a1ece978fc49249913a2df
Bradley Moore <BradleyMooreTX@gmail.com>
2013-10-14 09:49:06 -0600

    remade deck printout in main, minor Hand tweaks

--- src/fivecardstud/FiveCardStud.java
+++ src/fivecardstud/FiveCardStud.java
@@ -4,6 +4,10 @@ public class FiveCardStud {
 
     public static void main(String[] args) {
         Deck deck = new Deck();
-        for (Card card: deck.deck) System.out.println(card. value + " " + card.rank + " " + card.suit);
+        
+        deck.shuffle();
+        for (Card card: deck.deck) {
+            System.out.println(card. value + " " + card.rank + " " + card.suit);
+        }
     }
 }
