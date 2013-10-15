996af799181801d81065ccd0bfac74d48f2e202e
Bradley Moore <BradleyMooreTX@gmail.com>
2013-10-14 22:31:49 -0600

    hands get dealt and are printing

--- src/fivecardstud/FiveCardStud.java
+++ src/fivecardstud/FiveCardStud.java
@@ -1,13 +1,18 @@
 package fivecardstud;
 
 public class FiveCardStud {
+    static Deck deck;    
+    static Status status;
 
     public static void main(String[] args) {
-        Deck deck = new Deck();
-        
+        deck = new Deck();
         deck.shuffle();
+        
+        status = new Status();
+
         for (Card card: deck.deck) {
-            System.out.println(card. value + " " + card.rank + " " + card.suit);
+            System.out.println(card.value + " " + card.rank + " " + card.suit);
         }
+        Status.showPlayers();
     }
 }
