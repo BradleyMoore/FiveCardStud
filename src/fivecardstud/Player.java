b61b66e435410eb770485c1c36017ac817c398ad
Bradley Moore <BradleyMooreTX@gmail.com>
2013-11-01 20:46:45 -0600

    refactor some; cards are brought out of Hand correctly

--- src/fivecardstud/Player.java
+++ src/fivecardstud/Player.java
@@ -1,11 +1,36 @@
 package fivecardstud;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
+    int playerNumber;
+    List<Card> cards;
     String name;
-    Card[] hand;
+    Hand hand;
     
-    public Player(String name, Card[] hand){
+    
+    public Player(int playerNumber, String name){
+        cards = new ArrayList<>();
+        this.playerNumber = playerNumber;
         this.name = name;
-        this.hand = hand;
+        this.hand = new Hand();
+    }
+    
+    public void deal(){
+        cards = hand.deal();
+    }
+    
+    public void show(){
+        for (Card card: cards){
+                System.out.println(this.name + " -- " + card.rank + " of " + card.suit);
+        }
+        System.out.println("");
+    }
+    
+    public static void createAll(){
+        // create players based on numPlayers
+        for (int i=0; i<Status.numPlayers; i++) {
+            FiveCardStud.status.players.add(new Player(i+1, "Player " + Integer.toString(i+1)));
+        }
     }
 }
