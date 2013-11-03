ae4a98435f4de8a5eb7944fd91c37062fbd56cf9
Bradley Moore <BradleyMooreTX@gmail.com>
2013-11-03 00:15:01 -0600

    most likely completes Ranking.checkPairs()

--- src/fivecardstud/Ranking.java
+++ src/fivecardstud/Ranking.java
@@ -30,12 +30,13 @@ public class Ranking {
     }
     
     static List<Card> checkStrait(List<Card> cards){
+        Collections.sort(cards, new ValueComparator());
+
         boolean skipIter;
         Card temp;
+        
         skipIter = true;
         temp = cards.get(0);
-        
-        Collections.sort(cards, new ValueComparator());
         
         for (Card card: cards) {
             // skip first iteration
@@ -51,13 +52,63 @@ public class Ranking {
         return cards;
     }
 
-    
-    
-    
-    
-    // rank hands from 0 being the worst and up to the best
-    public int evaluate(List<Card> cards){
-
-        return this.rank;
+    static List<List<Card>> checkPairs(List<Card> cards) {
+        Collections.sort(cards, new ValueComparator());
+        
+        boolean skipIter;
+        boolean foundPair;
+        Card temp;
+        List<Card> matches1;
+        List<Card> matches2;
+        List<List<Card>> pairs;
+        
+        foundPair = false;
+        skipIter = true;
+        temp = cards.get(0);
+        matches1 = new ArrayList<>();
+        matches2 = new ArrayList<>();
+        pairs = new ArrayList<>();
+        
+        for (Card card: cards) {
+            // skip first iteration
+            if (skipIter == true) {
+                skipIter = false;
+                continue;
+            }
+            
+            // if matches2 HAS NOT been used keep adding pairs to matches1
+            if (matches2.isEmpty() && temp.value == card.value) {
+                if (temp == cards.get(0)) matches1.add(temp);
+                matches1.add(card);
+                foundPair = true;
+            // if matches1 HAS been used keep adding pairs to matches 2
+            } else if (foundPair == false && !matches1.isEmpty() && temp.value == card.value) {
+                matches2.add(card);
+            } else {
+                foundPair = false;
+            }
+            temp = card;
+        }
+        
+        // add non-empty lists to returned list of lists
+        if (!matches1.isEmpty()) {
+            pairs.add(matches1);
+        } else {
+            pairs.add(null);
+        }
+        
+        if (!matches2.isEmpty()) {
+            pairs.add(matches2);
+        } else {
+            pairs.add(null);
+        }
+        
+        return pairs;
     }
+    
+    static void evaluate(){ 
+        
+    }
+    
+
 }
